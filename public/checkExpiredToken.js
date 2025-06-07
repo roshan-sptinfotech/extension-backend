636f45b5587f506e0b1d0532c3eb469670fb6bac
roshan <roshan@sptinfotech.com>
2025-06-07 16:12:44 +0530

    Remove public folder

--- public/checkExpiredToken.js
+++
@@ -1,8 +0,0 @@
-//This is supposed to be used inside the <iframe> element's scripts
-function checkExpiredToken(jsonResponse)
-{
-    if(jsonResponse.expired)
-        localStorage.removeItem("token");
-
-    return jsonResponse.expired ? true: false;
-}
