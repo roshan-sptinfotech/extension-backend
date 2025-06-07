d566bcc6ec256b2c4e5d1f9730077bbbae28c31d
roshan <roshan@sptinfotech.com>
2025-06-07 15:18:14 +0530

    first commit

--- database/connection.js
+++ database/connection.js
@@ -1,17 +1,26 @@
-const mongodb = require("mongodb");
-const serverApiVersion = mongodb.ServerApiVersion;
 
-const connectionURL = process.env.MONGODB_SERVER_URL;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sptvivek814:y10iwMvhjm0qGCft@cluster0.quacet5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const clientOptions = {
-    serverApi: 
-    {
-        version: serverApiVersion.v1,
-        strict: true,
-        deprecationErrors: true
-    }
-};
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-const mongoClient = new mongodb.MongoClient(connectionURL, clientOptions);
-
-module.exports = mongoClient;
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
